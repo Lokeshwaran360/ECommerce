f717569cbfa3fefc4b449d7fff12a4728bc7cb43
Lokeshwaran360 <babybala1998@gmail.com>
2023-12-26 18:58:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import List from './Store/List';
+
+
+function App() {
+ 
+ 
+ 
+  return (
+    <>
+     <List/>
+    </>
+  )
+
+}
+    
+
+export default App;
